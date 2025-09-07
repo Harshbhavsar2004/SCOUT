6583e8fceac4cd0932214286b187fd0417f6e01e
Harshbhavsar2004 <hbhavsar847@gmail.com>
2025-09-07 18:19:46 +0530

    updated excalidraw

--- src/app/student/draw/page.jsx
+++ src/app/student/draw/page.jsx
@@ -1,23 +1,13 @@
 "use client";
-
 import dynamic from "next/dynamic";
 import "@excalidraw/excalidraw/index.css";
 
-// Dynamically import Excalidraw only on the client
 const Excalidraw = dynamic(
-  () => import("@excalidraw/excalidraw").then((mod) => mod.Excalidraw),
+  async () => (await import("@excalidraw/excalidraw")).Excalidraw,
   {
     ssr: false,
-    loading: () => <p>Loading drawing board...</p>, // optional fallback
-  }
+  },
 );
-
-const ExcalidrawWrapper = () => {
-  return (
-    <div style={{ height: "100vh", width: "100vw" }}>
-      <Excalidraw />
-    </div>
-  );
-};
-
-export default ExcalidrawWrapper;
+export default function App() {
+  return <Excalidraw />;
+}

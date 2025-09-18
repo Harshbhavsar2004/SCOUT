770dadf8047d5e12a4f4cccf9cebca2bd31f1d9c
Harshal Bhavsar <154224487+Harshbhavsar2004@users.noreply.github.com>
2025-09-18 18:37:14 +0530

    Update page.jsx

--- src/app/coordinator/page.jsx
+++ src/app/coordinator/page.jsx
@@ -3,5 +3,5 @@ import { redirect } from "next/navigation";
 
 export default async function TeacherPage() {
   if (!(await checkRole("coordinator"))) redirect("/");
-  return <p>Coordinator Dashboard</p>;
+  return <p>Coordinator Dashboard made by me</p>;
 }
